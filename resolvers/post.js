28aac41ca01bef0c08d129954c734f624fa408b3
Sehrishah <sehrishwaheed98@gmail.com>
2021-01-14 01:13:07 +0300

    Post Query and mutation, input type and query variables

--- resolvers/post.js
+++ resolvers/post.js
@@ -1,9 +1,41 @@
 const { gql } = require("apollo-server-express");
+const { posts } = require("../temp");
 
-const totalPosts = () => 42;
+/*--------------------------
+       Queries
+---------------------------*/
+
+// return length
+const totalPosts = () => posts.length;
+
+// return array of posts
+const allPosts = () => posts;
+
+/*--------------------------
+       Mutation
+---------------------------*/
+
+// parent -> parent mutation type e.g newPost
+// args -> e.g args.title but you can also destructure it
+const newPost = (parent, args, context) => {
+  // create a new post Object
+
+
+  const post = {
+    id: posts.length + 1,
+    ...args.input,
+  };
+  // push new post object to posts array
+  posts.push(post);
+  return post;
+};
 
 module.exports = {
   Query: {
     totalPosts,
+    allPosts,
+  },
+  Mutation: {
+    newPost,
   },
 };
