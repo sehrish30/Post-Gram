4f108b893455e3deb723cf115a4ffd13ef98d7e8
Sehrishah <sehrishwaheed98@gmail.com>
2021-01-13 17:28:16 +0300

    Setup express server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+// use environmental variables
+require("dotenv").config();
+
+// create express server and invoke it
+const app = express();
+
+// rest endpoint
+app.get("/rest", (req, res) => {
+  res.json({
+    data: "you hit rest endpoint",
+  });
+});
+
+// listen app on port number
+app.listen(process.env.PORT, () => {
+  console.log(`server is read at http:// ${process.env.PORT}`);
+});
+
+// Also handle start script in package.json
