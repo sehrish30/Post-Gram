da4f91a7c8409e402e2aa21563f2d24bd66bc6b3
Sehrishah <sehrishwaheed98@gmail.com>
2021-01-15 12:26:02 +0300

    Design register page

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,8 +1,28 @@
+import { useState } from "react";
+
+const handleSubmit = () => {};
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+
   return (
     <div className="container">
       <div className="row p-5">
         <h4>Login</h4>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label>Email Address</label>
+            <input
+              className="form-control"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Enter email"
+              disabled={loading}
+            />
+          </div>
+        </form>
       </div>
     </div>
   );
--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,9 +1,31 @@
+import { useState } from "react";
+import "../../css/register.scss";
+
+const handleSubmit = () => {};
+
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
   return (
-    <div className="container">
-      <div className="row p-5">
-        <h4>Register</h4>
-      </div>
+    <div className=" p-5 register text-center body">
+      <h4>Register</h4>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Email Address</label>
+          <input
+            className="form-control"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            disabled={loading}
+          />
+        </div>
+        <div className="text-center">
+          <button className="mt-4 box text-center" disabled={!email || loading}>
+            Submit
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
